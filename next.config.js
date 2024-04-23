507d72d0cb55e774d53e57927fbe6f08fce0f388
thewitcher745 <thewitcher745@gmail.com>
2024-04-23 14:56:48 +0330

    FEAT: Set up GH Pages compatibility

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/next-js-review",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+};
+
+module.exports = nextConfig;
